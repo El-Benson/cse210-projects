92bb89001d0fa8d7df14ad2bc40eede9443b6784
El-Benson <122732891+El-Benson@users.noreply.github.com>
2024-02-24 12:21:30 +0100

    Update Program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,21 +1,47 @@
 using System;
 
-namespace NameRepeater
+namespace GradeCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("What is your first name?");
-            string firstName = Console.ReadLine();
+            // Ask the user for their grade percentage
+            Console.WriteLine("Enter your grade percentage:");
+            double gradePercentage = Convert.ToDouble(Console.ReadLine());
 
-            
-            Console.WriteLine("What is your last name?");
-            string lastName = Console.ReadLine();
+            // Determine the letter grade
+            char letter;
+            if (gradePercentage >= 90)
+            {
+                letter = 'A';
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = 'B';
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = 'C';
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = 'D';
+            }
+            else
+            {
+                letter = 'F';
+            }
 
-            
-            Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+            // Determine if the user passed the course
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine($"Congratulations! You passed the course with a grade of {letter}.");
+            }
+            else
+            {
+                Console.WriteLine($"Keep it up! You'll do better next time. Your grade is {letter}.");
+            }
         }
     }
 }
