4898910225baf4c641c7135a009775a6fd954cf3
El-Benson <122732891+El-Benson@users.noreply.github.com>
2024-02-24 12:43:58 +0100

    Update Program.cs

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,54 @@
 using System;
 
-class Program
+namespace GuessMyNumber
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep3 World!");
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            
+            int magicNumber = random.Next(1, 101);
+
+            int guess;
+            int attempts = 0;
+
+            
+            do
+            {
+                
+                Console.WriteLine("What is your guess?");
+                guess = Convert.ToInt32(Console.ReadLine());
+                attempts++;
+
+                
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            } while (guess != magicNumber);
+
+            
+            Console.WriteLine($"You made {attempts} guesses.");
+
+            
+            Console.WriteLine("Do you want to play again? (yes/no)");
+            string playAgain = Console.ReadLine().ToLower();
+
+            
+            if (playAgain == "yes")
+            {
+                Main(args);
+            }
+        }
     }
 }
